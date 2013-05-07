3e5a6c7db654c39ee4f8032be59434fdf9bd78c8
Shaun Luttin <shaunluttin@gmail.com>
2013-05-07 13:20:02 -0700

    Add a custom admin theme, and add an Index.cshtml file to override the default dashboard.

---
+++ orchard1x/src/Orchard.Web/Themes/MyAdmin/OverrideTheAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Routing;
+using Orchard.Themes;
+using Orchard.UI.Admin;
+
+namespace MyAdmin
+{
+    public class OverrideTheAdmin : IThemeSelector
+    {
+        public ThemeSelectorResult GetTheme(RequestContext context)
+        {
+            if (AdminFilter.IsApplied(context))
+            {
+                return new ThemeSelectorResult { Priority = 110, ThemeName = "MyAdmin" };
+            }
+
+            return null;
+        }
+    }
+
+}
