295da0b30c4fec36852a962ec8bf3e6977feedd6
Shaun Luttin <shaunluttin@gmail.com>
2013-05-08 08:12:32 -0700

    Create the DealerDashboard and get it displaying HelloWorld.

---
+++ orchard1x/src/Orchard.Web/Modules/BigFont.DealerDashboard/Routes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace BigFont.DealerDashboard
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 5,
+                    Route = new Route(
+                        "DealerDashboard",
+                        new RouteValueDictionary {
+                            {"area", "BigFont.DealerDashboard"},
+                            {"controller", "Home"},
+                            {"action", "Index"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "BigFont.DealerDashboard"}
+                        },
+                        new MvcRouteHandler())
+                }
+            };
+        }
+    }
+}
