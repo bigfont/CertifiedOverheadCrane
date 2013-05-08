a200fb4877e52ece0d390272054b2977079906a3
Shaun Luttin <shaunluttin@gmail.com>
2013-05-07 21:58:32 -0700

    Add a module that is going to attempt to modify the dashboard so that Dealers can CRUD only their own content items.

---
+++ orchard1x/src/Orchard.Web/Modules/BigFont.DashboardUsability/DashboardUsabilityHandler.cs
@@ -0,0 +1,41 @@
+﻿using Orchard.ContentManagement.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.DashboardUsability.Handlers
+{
+    public class DashboardUsabilityHandler : IContentHandler
+    {
+        public DashboardUsabilityHandler() { }
+        public void Activating(ActivatingContentContext context) { }
+        public void Activated(ActivatedContentContext context) { }
+        public void Initializing(InitializingContentContext context) { }
+        public void Creating(CreateContentContext context) { }
+        public void Created(CreateContentContext context) { }
+        public void Loading(LoadContentContext context) { }
+        public void Loaded(LoadContentContext context) { }
+        public void Updating(UpdateContentContext context) { }
+        public void Updated(UpdateContentContext context) { }
+        public void Versioning(VersionContentContext context) { }
+        public void Versioned(VersionContentContext context) { }
+        public void Publishing(PublishContentContext context) { }
+        public void Published(PublishContentContext context) { }
+        public void Unpublishing(PublishContentContext context) { }
+        public void Unpublished(PublishContentContext context) { }
+        public void Removing(RemoveContentContext context) { }
+        public void Removed(RemoveContentContext context) { }
+        public void Indexing(IndexContentContext context) { }
+        public void Indexed(IndexContentContext context) { }
+        public void Importing(ImportContentContext context) { }
+        public void Imported(ImportContentContext context) { }
+        public void Exporting(ExportContentContext context) { }
+        public void Exported(ExportContentContext context) { }
+
+        public void GetContentItemMetadata(GetContentItemMetadataContext context) { }
+        public void BuildDisplay(BuildDisplayContext context) { }
+        public void BuildEditor(BuildEditorContext context) { }
+        public void UpdateEditor(UpdateEditorContext context) { }
+    }
+}
