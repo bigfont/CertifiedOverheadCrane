aac6daedadc775338ebcbbddae6309673f7d94a7
Shaun Luttin <shaunluttin@gmail.com>
2013-05-13 20:09:41 -0700

    Create a part that pins images, part #, and model # onto a dealer product.

--- orchard/src/Orchard.Web/Modules/BigFont.DealerDashboard/Migrations.cs
+++ orchard/src/Orchard.Web/Modules/BigFont.DealerDashboard/Migrations.cs
@@ -8,39 +8,52 @@ using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
 using BigFont.DealerDashboard.Models;
 
-namespace BigFont.DealerDashboard
-{
-    public class Migrations : DataMigrationImpl
-    {
+namespace BigFont.DealerDashboard {
+    public class Migrations : DataMigrationImpl {
 
-        public int Create()
-        {
-            // Creating table DealerProductRecord
+        public int Create() {
+
+            // Part - DealerProductPart
             SchemaBuilder.CreateTable("DealerProductRecord", table => table
                 .ContentPartRecord()
             );
 
             ContentDefinitionManager.AlterPartDefinition(
                 typeof(DealerProductPart).Name, cfg => cfg
-                    .Attachable()
-                    //.WithField("Image 1", field => field.OfType("ImageField"))
-                    //.WithField("Image 2", field => field.OfType("ImageField"))
-                    //.WithField("Part #", field => field.OfType("TextField"))
-                    //.WithField("Model #", field => field.OfType("TextField"))
-                    );
+                    .Attachable());
 
-            //ContentDefinitionManager.AlterTypeDefinition("DealerProduct", cfg => cfg
-            //    .WithPart("DealerProductPart")
-            //    .WithPart("CommonPart", cp => cp
-            //        .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false")
-            //        .WithSetting("DateEditorSettings.ShowDateEditor", "false"))
-
-            //    .WithPart("BodyPart")
-            //    .WithPart("TitlePart")
-            //        .Draftable()
-            //        .Creatable()); // createable is necessary to leverage the existing Contents Feature pemissions and views
-                
             return 1;
+        }
+
+        public int UpdateFrom1() {
+
+            // Part - CraneProductPart
+            SchemaBuilder.CreateTable("CraneProductRecord", table => table
+                .ContentPartRecord()
+            );
+
+            ContentDefinitionManager.AlterPartDefinition(
+                typeof(CraneProductPart).Name, cfg => cfg
+                    .WithField("Image 1", field => field.OfType("ImageField"))
+                    .WithField("Image 2", field => field.OfType("ImageField"))
+                    .WithField("Part #", field => field.OfType("TextField"))
+                    .WithField("Model #", field => field.OfType("TextField"))
+                    .Attachable());
+
+            // Type - CraneProduct
+            ContentDefinitionManager.AlterTypeDefinition("UsedCraneStock",
+                cfg => cfg
+                    .WithPart("DealerProductPart")
+                    .WithPart("CraneProductPart")
+                    .WithPart("CommonPart", cp => cp
+                        .WithSetting("OwnerEditorSettings.ShowOwnerEditor", "false")
+                        .WithSetting("DateEditorSettings.ShowDateEditor", "false"))
+                    .WithPart("BodyPart")
+                    .WithPart("TitlePart")
+                        .Draftable()
+                        .Creatable()); 
+
+            return 2;
         }
     }
 }
--- orchard/src/Orchard.Web/Modules/BigFont.DealerDashboard/Models/DealerProduct.cs
+++ orchard/src/Orchard.Web/Modules/BigFont.DealerDashboard/Models/DealerProduct.cs
@@ -1,16 +1,20 @@
 ﻿using Orchard.ContentManagement;
 using Orchard.ContentManagement.Records;
+using Orchard.Environment.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace BigFont.DealerDashboard.Models
-{
-    public class DealerProductRecord : ContentPartRecord
-    {
+namespace BigFont.DealerDashboard.Models {
+    public class DealerProductRecord : ContentPartRecord {
     }
-    public class DealerProductPart : ContentPart<DealerProductRecord>
-    { 
+    public class DealerProductPart : ContentPart<DealerProductRecord> {
+    }
+    [OrchardFeature("BigFont.DealerDashboard.StandardTypes")]
+    public class CraneProductRecord : ContentPartRecord {
+    }
+    [OrchardFeature("BigFont.DealerDashboard.StandardTypes")]
+    public class CraneProductPart : ContentPart<CraneProductRecord> {
     }
 }
