123a7e596276ae9d8228299ce6cbfe46774efbe2
Shaun Luttin <shaunluttin@gmail.com>
2013-05-14 09:50:45 -0700

    Add the routes that map ~/Dashboard to the BigFont.OpenDashboard home controller's list action.

---
+++ orchard/src/Orchard.Web/Modules/BigFont.OpenDashboard/Routes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace BigFont.OpenDashboard
+{
+    public class Routes : IRouteProvider
+    {
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);            
+        }
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[] {
+                new RouteDescriptor {
+                    Name = "OpenDashboard",
+                    Priority = 5,
+                    Route = new Route(
+                        "Dashboard",
+                        new RouteValueDictionary {
+                            {"area", "BigFont.OpenDashboard"},
+                            {"controller", "Home"},
+                            {"action", "List"}
+                        },
+                        new RouteValueDictionary(),
+                        new RouteValueDictionary {
+                            {"area", "BigFont.OpenDashboard"}
+                        },
+                        new MvcRouteHandler())
+                }          
+            };
+        }
+    }
+}
