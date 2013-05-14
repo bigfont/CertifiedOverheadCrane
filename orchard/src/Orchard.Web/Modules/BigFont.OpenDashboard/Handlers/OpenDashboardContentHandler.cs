4f883d2d1c43dede12a36623f3e0f2483c9ff9bd
Shaun Luttin <shaunluttin@gmail.com>
2013-05-14 10:02:30 -0700

    Add the content handler to map specific urls to specific controller actions. Add the models for the opendashboardcontent.

---
+++ orchard/src/Orchard.Web/Modules/BigFont.OpenDashboard/Handlers/OpenDashboardContentHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using BigFont.OpenDashboard.Models;
+using System.Web.Routing;
+
+namespace BigFont.OpenDashboard
+{
+    public class DealerProductHandler : ContentHandler
+    {
+        public DealerProductHandler(IRepository<OpenDashboardContentRecord> repository)
+        {
+            Filters.Add(StorageFilter.For(repository));
+        }
+
+        protected override void GetItemMetadata(GetContentItemMetadataContext context)
+        {
+            var dealerPart = context.ContentItem.As<OpenDashboardContentPart>();
+
+            if (dealerPart == null)
+                return;
+
+            /*
+             * These ensure that the Create, Edit, View, and Delete buttons
+             * point to the OpenDashboard instead of to TheAdmin.
+             */
+            context.Metadata.CreateRouteValues = new RouteValueDictionary {
+                    {"Area", "BigFont.OpenDashboard"},
+                    {"Controller", "Home"}, // this one was Admin
+                    {"Action", "Create"},
+                    {"Id", context.ContentItem.ContentType}
+                };
+            context.Metadata.EditorRouteValues = new RouteValueDictionary {
+                    {"Area", "BigFont.OpenDashboard"},
+                    {"Controller", "Home"}, // this one was Admin
+                    {"Action", "Edit"},
+                    {"Id", context.ContentItem.Id}
+                };
+            context.Metadata.DisplayRouteValues = new RouteValueDictionary {
+                    {"Area", "BigFont.OpenDashboard"},
+                    {"Controller", "Home"}, // this one was Item
+                    {"Action", "Display"},
+                    {"Id", context.ContentItem.Id}
+                };
+            context.Metadata.RemoveRouteValues = new RouteValueDictionary {
+                    {"Area", "BigFont.OpenDashboard"},
+                    {"Controller", "Home"}, // this one was Admin
+                    {"Action", "Remove"},
+                    {"Id", context.ContentItem.Id}
+                };                        
+        }
+    }
+}
---
+++ orchard/src/Orchard.Web/Modules/BigFont.OpenDashboard/Models/OpenDashboardContent.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Records;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigFont.OpenDashboard.Models {
+    public class OpenDashboardContentRecord : ContentPartRecord {
+    }
+    public class OpenDashboardContentPart : ContentPart<OpenDashboardContentRecord> {
+    }
+    [OrchardFeature("BigFont.OpenDashboard.StandardContentTypes")]
+    public class ProductRecord : ContentPartRecord {
+    }
+    [OrchardFeature("BigFont.OpenDashboard.StandardContentTypes")]
+    public class ProductPart : ContentPart<ProductRecord> {
+    }
+}
