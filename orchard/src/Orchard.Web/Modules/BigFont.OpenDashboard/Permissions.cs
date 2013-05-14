e720d3b21edaa771af1a818f1046bae774cd7298
Shaun Luttin <shaunluttin@gmail.com>
2013-05-14 09:48:45 -0700

    Add the permissions that allow authenticated users to access the open dashboard.

---
+++ orchard/src/Orchard.Web/Modules/BigFont.OpenDashboard/Permissions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+
+namespace BigFont.OpenDashboard {
+    public class Permissions : IPermissionProvider {
+        public static readonly Permission ManageOpenDashboard = new Permission { Description = "Manage OpenDashboard", Name = "Manage OpenDashboard" };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new[] {
+                ManageOpenDashboard
+            };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+            return new[] {
+                new PermissionStereotype {
+                    Name = "Authenticated",
+                    Permissions = new[] {ManageOpenDashboard}
+                },
+            };
+        }
+    }
+}
+
